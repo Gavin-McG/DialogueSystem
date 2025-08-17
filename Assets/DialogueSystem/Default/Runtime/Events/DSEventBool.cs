4663a1b6727ed1374ff6869544c80f251c706331
GavinM5521 <gavinm5521@gmail.com>
2025-08-17 01:49:27 -0400

    Bool & Float event types

---
+++ Assets/DialogueSystem/Default/Runtime/Events/DSEventBool.cs
@@ -0,0 +1,8 @@
+﻿using DialogueSystem.Runtime;
+using UnityEngine;
+
+namespace DialogueSystem.Default.Runtime
+{
+    [CreateAssetMenu(fileName = "DialogueEvent", menuName = "Dialogue System/Events/Dialogue Event (Bool)")]
+    public class DSEventBool : DSEvent<bool> {}
+}
