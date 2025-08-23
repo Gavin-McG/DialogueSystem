29357096220448651f44d036ae43a27e129a44fb
GavinM5521 <gavinm5521@gmail.com>
2025-08-23 10:27:31 -0400

    Node base class for establishing contextNode

---
+++ Assets/DialogueSystem/Editor/Nodes/InitializeNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Unity.GraphToolkit.Editor;
+using UnityEditor.Experimental.GraphView;
+
+namespace WolverineSoft.DialogueSystem.Editor
+{
+    [Serializable, UseWithContext(typeof(RandomRedirectNode), typeof(SequentialRedirectNode))]
+    public abstract class InitializeNode : BlockNode
+    {
+        protected sealed override void OnDefineOptions(IOptionDefinitionContext context)
+        {
+            if (contextNode == null)
+                DialogueGraphUtility.AddNodeOption(context, "button", typeof(bool), displayName: "PRESS -->");
+            else
+                DefineFullOptions(context);
+        }
+
+        protected abstract void DefineFullOptions(IOptionDefinitionContext context);
+    }
+}
