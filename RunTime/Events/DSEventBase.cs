575f143fe8a52e3212ac2264c5dbe322c75d88c9
Gavin-McG <gavinmcg@umich.edu>
2026-06-15 21:05:02 -0400

    Update DSEventBase.cs

--- RunTime/Events/DSEventBase.cs
+++ RunTime/Events/DSEventBase.cs
@@ -3,5 +3,8 @@ using UnityEngine;
 
 namespace WolverineSoft.DialogueSystem
 {
+    /// <summary>
+    /// Base class for Generic and non-generic DSEvent classes
+    /// </summary>
     public abstract class DSEventBase : ScriptableObject {}
 }
